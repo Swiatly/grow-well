8c2c551a9656c3c99ae42b731596f6680753134b
Filip <filip.swiatly@yahoo.com>
2024-12-01 22:36:34 +0100

    added sign up page to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Components
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+
+export const routes: Routes = [
+    { path: 'register', component: SignUpComponent }
+];
