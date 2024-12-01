33f0072a225eaaba64a7092f14ce62bb08e83afb
Filip <filip.swiatly@yahoo.com>
2024-12-01 22:36:04 +0100

    created sign up page

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    imports: [],
+    templateUrl: './sign-up.component.html',
+    styleUrl: './sign-up.component.scss'
+})
+
+export class SignUpComponent {
+    public logoTextWhite: string = '../../../assets/logo/logo-text--white.svg';
+    public logoTextWhiteMobile: string = '../../../assets/logo/logo-text-horizontal--white.svg';
+}
+  
