6ed389ba14e69bc4730fa397d447dec2bbbc56cd
Filip <filip.swiatly@yahoo.com>
2024-12-03 19:39:47 +0100

    added reactive forms and validation to forms

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,14 +1,52 @@
+import { NgIf } from "@angular/common";
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from "@angular/forms";
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [
+        ReactiveFormsModule,
+        NgIf
+    ],
     templateUrl: './sign-up.component.html',
     styleUrl: './sign-up.component.scss'
 })
 
 export class SignUpComponent {
-    public logoTextWhite: string = '../../../assets/logo/logo-text--white.svg';
-    public logoTextWhiteMobile: string = '../../../assets/logo/logo-text-horizontal--white.svg';
+    public signUpForm: FormGroup;
+
+    constructor() {
+        this.signUpForm = new FormGroup({
+            'username': new FormControl(null, [Validators.required]),
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.pattern('^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')
+            ]),
+            'repeat-password': new FormControl(null, [Validators.required, this.matchPasswords.bind(this)])
+        })
+    }
+
+
+  private matchPasswords(control: FormControl): ValidationErrors | null {
+    if (!this.signUpForm) {
+      return null; 
+    }
+    const password = this.signUpForm.get('password')?.value;
+    const repeatPassword = control.value;
+
+    if (password !== repeatPassword) {
+      return { passwordsMismatch: true };
+    }
+    return null;
+  }
+
+    public onSignUp(): void {
+        if (this.signUpForm.invalid) {
+            return;
+        }
+        console.log('Form Submitted:', this.signUpForm.value);
+    }
 }
   
